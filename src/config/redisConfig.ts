f20dc3c355b6b110f5f0f5f3ba21ccd2c32f3c94
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-03-01 08:09:44 +0200

    configure session and redis

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,24 +1,24 @@
 import session from "express-session";
-// import { createClient } from "redis";
-// import RedisStore from "connect-redis";
+import { createClient } from "redis";
+import RedisStore from "connect-redis";
 
-// const redisClient = createClient({
-//   url: process.env.REDIS_URL,
-// });
-// redisClient.connect().catch(console.error);
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+redisClient.connect().catch(console.error);
 
-// const redisSrore = new RedisStore({ client: redisClient });
+const redisSrore = new RedisStore({ client: redisClient });
 
 export const sessionMiddleware = session({
-  // store: redisSrore,
+  store: redisSrore,
   secret: process.env.SESSION_SECRET || "secret",
   resave: false,
   saveUninitialized: false,
-  proxy: true,
-  name: "mybestcookie",
   cookie: {
+    httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    httpOnly: false,
-    maxAge: 1000 * 60 * 60,
+    sameSite: "none",
+    domain: ".onrender.com",
+    maxAge: 1000 * 60 * 60
   },
 });
